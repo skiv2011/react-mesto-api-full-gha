c0ab2f0f7ce4c96f10d7b9e6e28679178072c752
Pavel Zhukovskii <FiremanDzen@yandex.ru>
2023-07-04 00:00:12 +0300

    первая итерация исправления

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,6 +1,6 @@
 
-// export const BASE_URL = 'https://api.firemandzen.nomoredomains.monster';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.firemandzen.nomoredomains.monster';
+// export const BASE_URL = 'http://localhost:3000';
 
 const check= (res) => {
     if (res.ok) {
